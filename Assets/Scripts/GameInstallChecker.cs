8bc31f59cb128a42d8b26f66565f3969ba3febd7
TheSmallBlue <ilanmori@hotmail.com>
2024-09-05 20:12:07 -0300

    Separate install checker into its own class

---
+++ Assets/Scripts/GameInstallChecker.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Narod.SteamGameFinder;
+using System.IO;
+using System;
+
+public static class GameInstallChecker
+{
+    public static void GetPath(Action<string> result)
+    {
+        if (IsInstalledOnSteam(out string path))
+            result(path);
+        else
+            CreatePathCheckPopup("<b>Steam directory not found!</b> Please paste the path that contains the game's EXE file below, or use the file picker to find it", result);
+    }
+
+    static void CreatePathCheckPopup(string text, Action<string> result)
+    {
+        PopUpManager.Instance.Create<PathPopUp>()
+            .SetText<PathPopUp>(text)
+            .OnSubmit(pth =>
+            {
+                if (!HasGameExe(pth))
+                    CreatePathCheckPopup("<b>Folder did not contain the game's EXE file!</b> Either you picked the wrong folder, or you renamed the game's EXE. Both of these things are bad!", result);
+                else
+                    result(pth);
+            });
+    }
+
+    static bool IsInstalledOnSteam(out string path)
+    {
+        SteamGameLocator gameLocator = new SteamGameLocator();
+        path = "";
+
+        if (!gameLocator.getIsSteamInstalled()) return false;
+
+        try
+        {
+            path = gameLocator.getGameInfoByFolder("MetaWare High School (Demo)").steamGameLocation;
+        }
+        catch (DirectoryNotFoundException)
+        {
+            return false;
+        }
+
+        if (!HasGameExe(path)) return false;
+
+        return true;
+    }
+
+    static bool HasGameExe(string pathToFolder) => File.Exists(pathToFolder + "/MetaWareHighSchoolDemo.exe");
+}
--- Assets/SetUp.cs
+++ Assets/SetUp.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Narod.SteamGameFinder;
-using System.IO;
 
 public class SetUp : MonoBehaviour
 {
@@ -10,10 +8,7 @@ public class SetUp : MonoBehaviour
     {
         // TODO: save/load file path
 
-        if(IsInstalledOnSteam(out string path))
-            ModFolderCheck(path);
-        else
-            CreatePathCheckPopup("<b>Steam directory not found!</b> Please paste the path that contains the game's EXE file below, or use the file picker to find it");
+        GameInstallChecker.GetPath(pth => ModFolderCheck(pth));
     }
 
     void ModFolderCheck(string gamePath)
@@ -21,39 +16,5 @@ public class SetUp : MonoBehaviour
         Debug.Log("Valid game folder!");
     }
 
-    void CreatePathCheckPopup(string text)
-    {
-        PopUpManager.Instance.Create<PathPopUp>()
-            .SetText<PathPopUp>(text)
-            .OnSubmit(pth =>
-            {
-                if (!HasGameExe(pth))
-                    CreatePathCheckPopup("<b>Folder did not contain the game's EXE file!</b> Either you picked the wrong folder, or you renamed the game's EXE. Both of these things are bad!");
-                else 
-                    ModFolderCheck(pth);
-            });
-    }
-
-    bool IsInstalledOnSteam(out string path)
-    {
-        SteamGameLocator gameLocator = new SteamGameLocator();
-        path = "";
-
-        if(!gameLocator.getIsSteamInstalled()) return false;
-
-        try
-        {
-            path = gameLocator.getGameInfoByFolder("MetaWare High School (Demo)").steamGameLocation;
-        }
-        catch (DirectoryNotFoundException)
-        {
-            return false;
-        }
-
-        if(!HasGameExe(path)) return false;
-
-        return true;
-    }
-
-    bool HasGameExe(string pathToFolder) => File.Exists(pathToFolder + "/MetaWareHighSchoolDemo.exe");
+    
 }
